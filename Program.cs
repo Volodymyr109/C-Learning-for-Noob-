052f280118571a93477c5429d979caf0a0085d8b
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-10-12 22:36:01 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace lernen {
+
+    class Program {
+
+        static void Main() {
+
+            int a = 5;
+            int b = 7;
+            bool ishappy = true;
+            
+            if (a > 7){
+                System.Console.WriteLine("Num > 7");
+                // Operator &&  -  "and"
+            } else if (a == 5 && ishappy == false) {
+                System.Console.WriteLine("Num a = 5 and I am happy");
+                // Operator ||  -  "or"
+            } else if (a < b || ishappy == true) {
+                System.Console.WriteLine("5 is < 7 and I am happy");
+            } else {
+                System.Console.WriteLine("Num < 7");
+            }
+        }
+    }
+}
