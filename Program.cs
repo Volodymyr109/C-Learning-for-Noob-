1727361c8b237264bbf260a8803ff10502e41747
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-10-13 15:57:59 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            double a, b;
+            char c;
+            Console.WriteLine("Enter first num: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("What do yu want, + - * / ???");
+            c = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine("Enter second num: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            switch(c) {
+                case '+':
+                    Console.WriteLine("{0}+{1}={2}", a, b, a + b);
+                    break;
+                case '-':
+                    Console.WriteLine("{0}-{1}={2}", a, b, a - b);
+                    break;
+                case '*':
+                    Console.WriteLine("{0}*{1}={2}", a, b, a * b);
+                    break;
+                case '/':
+                    Console.WriteLine("{0}/{1}={2}", a, b, a / b);
+                    break;
+                default:
+                    Console.WriteLine("Feller!! Error! ");
+                    break;
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
