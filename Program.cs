234962486862abe6465e4a22960afc3262c868dc
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-10-13 22:32:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace lernen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = new int[10];
+
+            for (int i = 0; i < arr.Length; i++) {
+                Random random = new Random();
+                arr[i] = random.Next(1, 99);
+                System.Console.WriteLine("Element" + i + " :" + arr[i]);
+            }
+
+        }
+    }
+}
