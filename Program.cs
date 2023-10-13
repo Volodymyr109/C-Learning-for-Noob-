0f3a0e439564b5c7dc8baf99638076dc598cfb99
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-10-13 16:43:17 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Rextester
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+//           
+            // int i = 10;
+            // while (i >= 1) {
+            //     Console.WriteLine("El. {0}:", i);
+            //     i--;
+            // }
+//
+            // bool isHappy = true;
+            // while(isHappy) {
+            //     string end = Console.ReadLine();
+            //     if(end == "end"){
+            //         isHappy = false;
+            //         System.Console.WriteLine("NO HAPPY");
+            //     }
+            // }
+//
+            // int i = 100;
+            // do {
+            //     System.Console.WriteLine("El.: " + i);
+            // } while( i < 10);
+// bsp mit gerade zahlen in schleife
+            for (int i = 0; i < 10; i++) {
+                //if (i % 2 == 0) 
+                //    continue;
+                if (i % 3 == 0)
+                    continue;
+                Console.WriteLine("Element: " + i);
+            }
+
+        }
+    }
+}
