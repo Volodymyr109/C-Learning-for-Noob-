79501b6ab86456495690b15bce0cbe2d0d597e47
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-10-14 16:27:47 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+namespace lernen
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            List<int> li = new List<int> () {
+                1, 2, 3
+            };
+            li.Add(5);
+            li.Add(7);
+            li.Add(6);
+            li.Remove(1);
+            li.Sort();
+            //li.Reverse();
+            li.Clear();
+
+            foreach(int el in li) {
+                System.Console.WriteLine("El.:" + el);
+            }
+        }
+    }
+}
