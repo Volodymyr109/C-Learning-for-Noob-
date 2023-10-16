0b90b2944b0ace1ee120028e62ce54d8cb134a44
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-10-16 19:25:11 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿﻿using System;
+
+namespace lernen {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+            
+            string word = "MOIN MOIN ";
+            word += "WILLST DU EIN FISCHBROTSCHEN";
+
+            //System.Console.WriteLine(word.Length);
+            //word = String.Concat(word, "?????????");
+            //Console.WriteLine(string.Compare(word, "Helllo"));
+
+            string people = " Alex, Bob, Alice";
+            string[] names = people.Split(new char[]{','});
+            foreach(string el in names)
+                System.Console.WriteLine(el);
+        }
+
+    }
+}
