5e1fa20d9aebf70692a1d5cd1f2fbbce1ae36c40
Volodymyr Bohatyrchuk <100191534+Volodymyr109@users.noreply.github.com>
2023-10-16 09:52:32 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿﻿using System;
+
+namespace lernen {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+            
+            int[] arr = {1, 4, 5, 6, 7};
+            int res1 = summa(arr);
+            Console.WriteLine("res: " + res1);
+        }
+
+        public static int summa(int[] arr) {
+
+            int summ = 0;
+            foreach(int el in arr)
+                summ += el;
+
+            return summ;
+        }
+
+    }
+}
